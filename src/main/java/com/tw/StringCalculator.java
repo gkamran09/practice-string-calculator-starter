2c705623a4abee4ef154578ea243b734fb820323
Gerard Kamran Kalambacal <kamrangerardkalambacal@gmail.com>
2023-08-10 10:04:05 +0800

    Included code implementation for the test cases

--- src/main/java/com/tw/StringCalculator.java
+++ src/main/java/com/tw/StringCalculator.java
@@ -2,6 +2,23 @@ package com.tw;
 
 public class StringCalculator {
     public int add(String string) {
-        return 0;
+        if(string.isEmpty()){
+            return 0;
+        }
+        String comma = ",";
+        if (string.startsWith("//")) {
+            int newLineIndex = string.indexOf("\n");
+            comma = string.substring(2, newLineIndex);
+            string = string.substring(newLineIndex + 1);
+        }
+
+        String[] numbers = string.split("[\n" + comma + "]");
+        int sum = 0;
+
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+
+        return sum;
     }
 }
